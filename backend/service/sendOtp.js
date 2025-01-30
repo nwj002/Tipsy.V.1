706bf2217b9b76602bc050c1c2180ef665018256
nwj002 <nwj.shrestha@gmail.com>
2025-01-30 23:58:33 +0545

    Minor Bug fix

--- backend/service/sendOtp.js
+++ backend/service/sendOtp.js
@@ -1,31 +1,33 @@
-const axios = require('axios')
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
 
 const sendOtp = async (phone, otp) => {
-
-    // setting state
     let isSend = false;
 
-    // Url to send otp
-    const url = 'https://api.managepoint.co/api/sms/send'
+    // Get API details from .env
+    const url = process.env.SMS_API_URL;
+    const apiKey = process.env.SMS_API_KEY;
 
-    // payload to send
+    // Payload to send
     const payload = {
-        'apiKey': '4dc024b3-c09b-4c8d-87f2-f5f2158ed0fe',
+        'apiKey': apiKey,
         'to': phone,
         'message': `Your verification code is ${otp}`
-    }
+    };
 
     try {
-        const res = await axios.post(url, payload)
+        const res = await axios.post(url, payload);
         if (res.status === 200) {
             isSend = true;
         }
-
     } catch (error) {
-        console.log('Error Sending OTP', error.message)
+        console.log('Error Sending OTP', error.message);
     }
 
     return isSend;
-}
+};
 
 module.exports = sendOtp;
